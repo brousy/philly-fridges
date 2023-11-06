021736d53d5aff57add428fb53b2a23d2b6a1bef
Elizabeth Quinn <brousy2222@gmail.com>
2023-11-06 12:57:38 -0500

    header fixedon loegin

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -4,11 +4,11 @@ const Header = () => {
   return (
     <header className="bg-primary text-light mb-4 py-3">
       <div className="container justify-space-between-lg ">
-        <div className='row justify-center align-items-center'>
-        <Link className="col text-light" to="/">
+        <div className='row justify-content-between align-items-center'>
+        <Link className="col-auto text-light" to="/">
           <h1 className="m-0">Philly Fridges</h1>
         </Link>
-        <div className='col'>
+        <div className='col-auto'>
         <span className="m-0">Feeding our community.</span>
         </div>
         </div>
