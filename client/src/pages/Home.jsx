5c56c9ac118b7da0df41b39e721126ac3eac0966
Julien <larroquejl@gmail.com>
2023-11-08 18:57:36 -0500

    fixed fridge list not rendering updated list

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,28 +1,14 @@
-import { useQuery } from "@apollo/client";
-
 import FridgeList from "../components/FridgeList/fridgelist.jsx";
 
-import { QUERY_FRIDGES } from '../utils/queries';
-
 const Home = () => {
-    const { loading, data } = useQuery(QUERY_FRIDGES);
-    const fridge = data?.fridges || [];
-
-    console.log(fridge)
-
-    return (
-        
+    
+    return (       
         
         <main>
             <div className="flex-row justify-center">
                 <div>
                     <div className="col-12 col-md-8 mb-3">
-                        {loading ? (
-                            <div>Loading...</div>
-                        ) : (
-                            <FridgeList
-                                fridges={fridge} />
-                        )}
+                        <FridgeList />       
                     </div>
                 </div>
             </div>
