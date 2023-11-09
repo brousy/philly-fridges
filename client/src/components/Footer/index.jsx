7771e2eaf4233de670152dad44671f54e6fe22df
Julien <larroquejl@gmail.com>
2023-11-09 02:12:42 -0500

    lot of optional styling

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -6,17 +6,18 @@ const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="">
-      <div className=" bg-primary">
+    <footer>
+      <div className='text-center text-dark'>
         {location.pathname !== '/' && (
           <button
-            className="btn btn-dark mb-3"
+            className="btn bg-idie text-peach mb-3"
             onClick={() => navigate(-1)}
           >
             &larr; Go Back
           </button>
-        )}
-        
+        )}     
+        <p>Maintained by brousy with    
+        <img className="inline-block p-1" src="/love.png" width="5%" ></img>  </p>
       </div>
     </footer>
   );
