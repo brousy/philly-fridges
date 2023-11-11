90dd6e8254b244fde90f58be3c9e9c033a1d0f2e
Julien <larroquejl@gmail.com>
2023-11-11 15:35:49 -0500

    added take one button and functionality

--- client/src/components/ItemForm/index.jsx
+++ client/src/components/ItemForm/index.jsx
@@ -70,68 +70,68 @@ const ItemForm = ({ name }) => {
         <form onSubmit={handleFormSubmit}>
 
           <h2 className=" text-primary ">Add an Item to the Fridge</h2>
-            <div className="form-group ">
-              <label htmlFor="itemName">Name:</label>
-              <input
-                type="text"
-                id="itemName"
-                name="itemName"
-                value={formData.name}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="form-group ">
-              <label htmlFor="itemQuantity">Quantity:</label>
-             <input
-                type="number"
-                id="itemQuantity"
-                name="itemQuantity"
-                value={formData.itemQuantity}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <span>Is Frozen?</span>
-              <input
-                className="form-check-input"
-                type="radio"
-                name="isFrozen"
-                value="true"
-                id="flexRadioDefault1"
-                checked={formData.isFrozen === 'true'}
-                onChange={handleInputChange}
-              />
-              <label
-                className='form-check-label'
-                htmlFor="flexRadioDefault1"
-              >
-                True
-              </label>
-              <input
-                className="form-check-input"
-                type="radio"
-                name="isFrozen"
-                value="false"
-                id="flexRadioDefault2"
-                checked={formData.isFrozen === 'false'}
-                onChange={handleInputChange}
-              />
-              <label
-                className='form-check-label'
-                htmlFor="flexRadioDefault2"
-              >
-                False
-              </label>
-            </div>
-            <div className="form-group">
-              <DatePicker id="datePicker" selected={expiryDate} onChange={(date) => setExpiryDate(date)} />
-              <label
-                className='form-check-input' id='datePicker'>Expiration Date
-              </label>
-                {error && <p className="error">{error.message}</p>}
-             </div>
-             <button type="submit">Add Item</button>
-         </form>
+          <div className="form-group ">
+            <label htmlFor="itemName">Name:</label>
+            <input
+              type="text"
+              id="itemName"
+              name="itemName"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group ">
+            <label htmlFor="itemQuantity">Quantity:</label>
+            <input
+              type="number"
+              id="itemQuantity"
+              name="itemQuantity"
+              value={formData.itemQuantity}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <span>Is Frozen?</span>
+            <input
+              className="form-check-input"
+              type="radio"
+              name="isFrozen"
+              value="true"
+              id="flexRadioDefault1"
+              checked={formData.isFrozen === 'true'}
+              onChange={handleInputChange}
+            />
+            <label
+              className='form-check-label'
+              htmlFor="flexRadioDefault1"
+            >
+              True
+            </label>
+            <input
+              className="form-check-input"
+              type="radio"
+              name="isFrozen"
+              value="false"
+              id="flexRadioDefault2"
+              checked={formData.isFrozen === 'false'}
+              onChange={handleInputChange}
+            />
+            <label
+              className='form-check-label'
+              htmlFor="flexRadioDefault2"
+            >
+              False
+            </label>
+          </div>
+          <div className="form-group">
+            <DatePicker id="datePicker" selected={expiryDate} onChange={(date) => setExpiryDate(date)} />
+            <label
+              className='form-check-label' htmlFor='datePicker'>Expiration Date
+            </label>
+            {error && <p className="error">{error.message}</p>}
+          </div>
+          <button type="submit">Add Item</button>
+        </form>
       ) : (
         <p>You need to be logged in to add items to the fridge.</p>
       )}
--- client/src/components/ItemList/index.jsx
+++ client/src/components/ItemList/index.jsx
@@ -1,8 +1,56 @@
-const ItemList = ({ items = [], onTakeOne }) => {
-  if (!items.length) {
+import { useQuery, useMutation } from '@apollo/client';
+
+import { UPDATE_ITEM } from '../../utils/mutations';
+import { QUERY_FRIDGE_ITEMS } from "../../utils/queries";
+
+
+
+const ItemList = ({ id }) => {
+
+  console.log( id )
+
+
+  const { loading, data } = useQuery(QUERY_FRIDGE_ITEMS, {
+    variables: { fridgeId: id },
+  });
+
+  const fridge = data?.fridgeItems || {};
+
+  console.log(fridge)
+
+  const [updateOne, { error }] = useMutation(UPDATE_ITEM
+    
+    , {
+    refetchQueries: [
+      QUERY_FRIDGE_ITEMS,
+      'getFridgeItems'
+    ]
+  }
+  
+  );
+
+  const handleTakeOne = async (itemId, name, quantity) => {
+
+    const newQty = quantity-1
+
+    try {
+      const items = await updateOne({
+        variables: {
+          itemId: itemId,
+          name: name,
+          quantity: newQty,
+        },
+      })
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (loading) {
     return <h3>No items yet</h3>;
-  }        
-    return (
+  }
+  return (
 
     <div className='bg-idie col-12 p-4 mt-4 rounded border border-light'>
       <h2
@@ -12,18 +60,19 @@ const ItemList = ({ items = [], onTakeOne }) => {
         Items
       </h2>
       <div className="p-4">
-        {items.map((item) => (
+        {fridge.items.map((item) => (
           <div key={item._id} className="col-12 mb-3 pb-3">
             <div className="p-3 bg-light text-dark rounded">
               <div className="d-flex justify-content-between">
-              <h5 className="card-header">
-                {item.itemName}
-              </h5>
-              <button className="content-right bg-dpurple text-light" onClick={() => handleTakeOne(item._id)}>Take One</button>
+                <h5 className="card-header">
+                  {item.itemName} {' '}
+                  Qty {' '} {item.itemQuantity}
+                </h5>
+                <button className="content-right bg-dpurple text-center text-light" onClick={() => handleTakeOne(item._id, item.itemName, item.itemQuantity)}>Take One</button>
               </div>
               <p className="text-center">
                 {item.itemUsername} {item.itemName} expires on {item.expiryDate}
-              </p>              
+              </p>
             </div>
 
           </div>
